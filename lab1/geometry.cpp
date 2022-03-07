8c3fe4fcbc0bd2620d6cd084bbdb0c6be9582284
Semen <miht.sem@yandex.com>
2022-03-07 11:29:45 +0300

    fixing problems

--- lab1/geometry.cpp
+++ lab1/geometry.cpp
@@ -25,9 +25,11 @@
 
 class Dot
 {
+private:
+    double x, y;
+
 public:
-    float x, y;
-    Dot(float x = 0, float y = 0) : x(x), y(y) {}
+    explicit Dot(const double &x = 0.0, const double &y = 0.0) : x(x), y(y) {}
     Dot(const Dot &dot) : x(dot.x), y(dot.y) {}
     Dot &operator=(const Dot &dot)
     {
@@ -37,6 +39,14 @@ public:
         y = dot.y;
         return *this;
     }
+    double getX() const
+    {
+        return this->x;
+    }
+    double getY() const
+    {
+        return this->y;
+    }
     friend std::ostream &operator<<(std::ostream &out, const Dot &dot)
     {
         return out << "(x: " << dot.x << ", y: " << dot.y << ")\n";
@@ -45,37 +55,63 @@ public:
 
 class Polyline
 {
+protected:
+    std::vector<Dot> m_dot_segments;
+
 public:
-    int size = 2;
-    std::vector<Dot> dot_segments;
-    Polyline(std::vector<Dot> dot_segments = {Dot(0.0, 0.0), Dot(1.0, 1.0)}, int size = 2) : dot_segments(dot_segments), size(size) {}
-    Polyline(const Polyline &polyline) : dot_segments(polyline.dot_segments), size(polyline.size) {}
+    explicit Polyline(const std::vector<Dot> &dot_segments = {Dot(0.0, 0.0), Dot(1.0, 1.0)}) : m_dot_segments(dot_segments) {}
+    Polyline(const Polyline &polyline) : m_dot_segments(polyline.m_dot_segments) {}
     Polyline &operator=(const Polyline &polyline)
     {
         if (&polyline == this)
             return *this;
-        dot_segments = polyline.dot_segments;
-        size = polyline.size;
+        this->m_dot_segments = polyline.m_dot_segments;
         return *this;
+    }
+    int size() const
+    {
+        return this->m_dot_segments.size();
+    }
+    double perimeter() const
+    {
+        double sum_ = 0;
+        for (int number_of_dot = 0; number_of_dot < m_dot_segments.size() - 1; number_of_dot++)
+        {
+            double start_x = m_dot_segments[number_of_dot].getX();
+            double start_y = m_dot_segments[number_of_dot].getY();
+            double end_x = m_dot_segments[number_of_dot + 1].getX();
+            double end_y = m_dot_segments[number_of_dot + 1].getY();
+            sum_ += sqrt(pow((end_x - start_x), 2) + pow((end_y - start_y), 2));
+        }
+        return sum_;
     }
 };
 
 class ClosedPolyline : public Polyline
 {
 public:
-    ClosedPolyline(std::vector<Dot> dot_segments = {Dot(0.0, 0.0), Dot(1.0, 1.0)}, int size_ = 2) : Polyline(dot_segments, size_)
+    explicit ClosedPolyline(std::vector<Dot> dot_segments = {Dot(0.0, 0.0), Dot(1.0, 1.0)})
     {
         dot_segments.push_back(dot_segments[0]);
-        size += 1;
+        m_dot_segments = dot_segments;
     }
-    ClosedPolyline(const ClosedPolyline &closed_polyline) : Polyline(closed_polyline.dot_segments, closed_polyline.size) {}
+    ClosedPolyline(const ClosedPolyline &closed_polyline) : Polyline(closed_polyline.m_dot_segments) {}
     ClosedPolyline &operator=(const ClosedPolyline &closed_polyline)
     {
         if (&closed_polyline == this)
             return *this;
-        dot_segments = closed_polyline.dot_segments;
-        size = closed_polyline.size;
+        m_dot_segments = closed_polyline.m_dot_segments;
         return *this;
+    }
+    Dot operator[](const int &index) const
+    {
+        if (index >= 0 && index < this->m_dot_segments.size())
+            return this->m_dot_segments[index];
+        else
+        {
+            std::cout << "INDEX OUT OF BOUNDS!!!";
+            exit(1);
+        }
     }
 };
 
@@ -85,8 +121,8 @@ protected:
     ClosedPolyline m_closed_polyline;
 
 public:
-    Polygon(ClosedPolyline closed_polyline = ClosedPolyline()) : m_closed_polyline(closed_polyline) {}
-    Polygon(std::vector<Dot> polyline = {Dot(0.0, 0.0), Dot(1.0, 1.0)}) : m_closed_polyline(ClosedPolyline(polyline, polyline.size())) {}
+    explicit Polygon(const ClosedPolyline &closed_polyline = ClosedPolyline()) : m_closed_polyline(closed_polyline) {}
+    explicit Polygon(const std::vector<Dot> &polyline) : m_closed_polyline(ClosedPolyline(polyline)) {}
     Polygon(const Polygon &polygon) : m_closed_polyline(polygon.m_closed_polyline) {}
     Polygon &operator=(const Polygon &polygon)
     {
@@ -95,41 +131,37 @@ public:
         m_closed_polyline = polygon.m_closed_polyline;
         return *this;
     }
-    float perimeter()
+    double perimeter() const
     {
-        float sum_ = 0;
-        for (int number_of_dot = 0; number_of_dot < m_closed_polyline.size - 1; number_of_dot++)
-        {
-            int start_x = m_closed_polyline.dot_segments[number_of_dot].x;
-            int start_y = m_closed_polyline.dot_segments[number_of_dot].y;
-            int end_x = m_closed_polyline.dot_segments[number_of_dot + 1].x;
-            int end_y = m_closed_polyline.dot_segments[number_of_dot + 1].y;
-            sum_ += sqrt(pow((end_x - start_x), 2) + pow((end_y - start_y), 2));
-        }
-        return sum_;
+        std::cout << name();
+        return this->m_closed_polyline.perimeter();
     }
-    float square()
+    virtual double square() const
     {
-        float square_sum = 0;
+        std::cout << name();
+        double square_sum = 0.0;
         int dot2 = 1;
-        for (int dot1 = 0; dot1 < m_closed_polyline.size - 2; dot1++)
+        for (int dot1 = 0; dot1 < this->m_closed_polyline.size() - 2; dot1++)
         {
-
-            int x1 = m_closed_polyline.dot_segments[dot1].x;
-            int y1 = m_closed_polyline.dot_segments[dot1].y;
-            int x2 = m_closed_polyline.dot_segments[dot2].x;
-            int y2 = m_closed_polyline.dot_segments[dot2].y;
+            double x1 = this->m_closed_polyline[dot1].getX();
+            double y1 = this->m_closed_polyline[dot1].getY();
+            double x2 = this->m_closed_polyline[dot2].getX();
+            double y2 = this->m_closed_polyline[dot2].getY();
             square_sum += ((x1 + x2) * (y1 - y2));
             dot2 += 1;
         }
-        return square_sum / 2.0;
+        return abs(square_sum) / 2.0;
+    }
+    virtual std::string name() const
+    {
+        return "Polygon: ";
     }
 };
 
 class Triangle : public Polygon
 {
 public:
-    Triangle(const Dot &a, const Dot &b, const Dot &c) : Polygon(ClosedPolyline({a, b, c}, 3)) {}
+    Triangle(const Dot &a, const Dot &b, const Dot &c) : Polygon({a, b, c}) {}
     Triangle(const Triangle &triangle) : Polygon(triangle.m_closed_polyline) {}
     Triangle &operator=(const Triangle &triangle)
     {
@@ -138,12 +170,20 @@ public:
         m_closed_polyline = triangle.m_closed_polyline;
         return *this;
     }
+    double square() const override
+    {
+        return Polygon::square();
+    }
+    std::string name() const override
+    {
+        return "Triangle: ";
+    }
 };
 
 class Trapezoid : public Polygon
 {
 public:
-    Trapezoid(const Dot &a, const Dot &b, const Dot &c, const Dot &d) : Polygon(ClosedPolyline({a, b, c, d}, 4)) {}
+    Trapezoid(const Dot &a, const Dot &b, const Dot &c, const Dot &d) : Polygon({a, b, c, d}) {}
     Trapezoid(const Trapezoid &trapezoid) : Polygon(trapezoid.m_closed_polyline) {}
     Trapezoid &operator=(const Trapezoid &trapezoid)
     {
@@ -152,12 +192,20 @@ public:
         m_closed_polyline = trapezoid.m_closed_polyline;
         return *this;
     }
+    double square() const override
+    {
+        return Polygon::square();
+    }
+    std::string name() const override
+    {
+        return "Trapezoid: ";
+    }
 };
 
 class RegularPolygon : public Polygon
 {
 public:
-    RegularPolygon(const Dot &a, const Dot &b, const Dot &c, const Dot &d) : Polygon(ClosedPolyline({a, b, c, d}, 4)) {}
+    RegularPolygon(const std::vector<Dot>& polyline = {Dot(0.0, 0.0), Dot(1.0, 1.0)}) : Polygon(polyline) {}
     RegularPolygon(const RegularPolygon &regularPolygon) : Polygon(regularPolygon.m_closed_polyline) {}
     RegularPolygon &operator=(const RegularPolygon &regularPolygon)
     {
@@ -166,19 +214,31 @@ public:
         m_closed_polyline = regularPolygon.m_closed_polyline;
         return *this;
     }
+    double square() const override
+    {
+        return Polygon::square();
+    }
+    std::string name() const override
+    {
+        return "Regular polygon: ";
+    }
 };
 
 int main()
 {
 
-    Dot a1(0, 0), a2(2, 3), a3(4, 0);
+    Dot a1(0.0, 0.0), a2(3.0, 3.0), a3(0.0, 7.0);
     Dot b1(0, 0), b2(0, 1), b3(2, 2), b4(1, 0);
     Dot c1(0, 0), c2(2, 3), c3(5, 3), c4(7, 0);
     Dot d1(0, 0), d2(0, 1), d3(1, 1), d4(1, 0);
-    std::vector<Polygon> polygon{Polygon({b1, b2, b3, b4}), Triangle(a1, a2, a3), Trapezoid(c1, c2, c3, c4), RegularPolygon(d1, d2, d3, d4)};
-    for (int i = 0; i < polygon.size(); i++)
+    Polygon *polygon = new Polygon({b1, b2, b3, b4});
+    Triangle *triangle = new Triangle(a1, a2, a3);
+    Trapezoid *trapezoid = new Trapezoid(c1, c2, c3, c4);
+    RegularPolygon *regular_polygon = new RegularPolygon({d1, d2, d3, d4});
+    std::vector<Polygon *> polygons{polygon, triangle, trapezoid, regular_polygon};
+    for (int i = 0; i < polygons.size(); i++)
     {
-        std::cout << "Perimeter: " << polygon[i].perimeter() << "; Square: " << polygon[i].square() << ";\n";
+        std::cout << "Perimeter " << polygons[i]->perimeter() << "; Square " << polygons[i]->square() << ";\n";
     }
 
     return 0;
--- lab1/polynomial.cpp
+++ lab1/polynomial.cpp
@@ -16,7 +16,7 @@
 #include <cmath>
 #include <algorithm>
 
-char plus_or_minus(int num)
+char plus_or_minus(double num)
 {
     return num > 0 ? '+' : '-';
 }
@@ -24,10 +24,10 @@ char plus_or_minus(int num)
 class Polynomial
 {
 private:
-    std::vector<int> m_coefficients;
+    std::vector<double> m_coefficients;
 
 public:
-    Polynomial(std::vector<int> coefficients = {}) : m_coefficients(coefficients) {}
+    Polynomial(const std::vector<double> &coefficients = {}) : m_coefficients(coefficients) {}
     Polynomial(const Polynomial &polynomial) : m_coefficients(polynomial.m_coefficients) {}
     ~Polynomial() { m_coefficients.clear(); }
     Polynomial &operator=(const Polynomial &polynomial)
@@ -40,7 +40,7 @@ public:
     friend std::ostream &operator<<(std::ostream &out, const Polynomial &polynomial)
     {
         out << "Your Polynomial: ";
-        int length_of_polynomial = polynomial.m_coefficients.size() - 1;
+        double length_of_polynomial = polynomial.m_coefficients.size() - 1;
         bool first = false;
         for (; length_of_polynomial > 1; length_of_polynomial--)
         {
@@ -71,7 +71,7 @@ public:
         }
         for (; length_of_polynomial > 1; length_of_polynomial--)
         {
-            int coefficient = polynomial.m_coefficients[length_of_polynomial];
+            double coefficient = polynomial.m_coefficients[length_of_polynomial];
             if (coefficient != 0 && abs(coefficient) != 1)
             {
                 out << plus_or_minus(coefficient) << " " << abs(coefficient) << "x^" << length_of_polynomial << " ";
@@ -98,7 +98,7 @@ public:
     }
     friend std::istream &operator>>(std::istream &in, Polynomial &polynomial)
     {
-        int n;
+        double n;
         in >> n;
         polynomial.m_coefficients.resize(n);
         for (int coefficients = 0; coefficients < n; coefficients++)
@@ -107,7 +107,7 @@ public:
         }
         return in;
     }
-    int &operator[](int index)
+    double &operator[](const int &index)
     {
         if (index >= this->m_coefficients.size())
         {
@@ -116,22 +116,31 @@ public:
         }
         return this->m_coefficients[index];
     }
-    bool operator==(const Polynomial &polynomial2)
+    bool operator==(const Polynomial &polynomial2) const
     {
-        return this->m_coefficients == polynomial2.m_coefficients;
+        if (this->m_coefficients.size() != polynomial2.m_coefficients.size())
+        {
+            return false;
+        }
+        for (int i = 0; i <= this->m_coefficients.size(); i++)
+        {
+            if (abs(polynomial2.m_coefficients[i] - this->m_coefficients[i]) > 0.001)
+                return false;
+        }
+        return true;
     }
-    bool operator!=(const Polynomial &polynomial2)
+    bool operator!=(const Polynomial &polynomial2) const
     {
         return !(this->m_coefficients == polynomial2.m_coefficients);
     }
-    Polynomial operator*(const int multiply)
+    Polynomial operator*(const double &multiply)
     {
         std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), this->m_coefficients.begin(), std::bind(std::multiplies<int>(), std::placeholders::_1, multiply));
         return *this;
     }
     Polynomial operator*(const Polynomial &polynomial2)
     {
-        std::vector<int> polynomial_multiply(this->m_coefficients.size() + polynomial2.m_coefficients.size() - 1);
+        std::vector<double> polynomial_multiply(this->m_coefficients.size() + polynomial2.m_coefficients.size() - 1);
         for (int i = 0; i < this->m_coefficients.size(); i++)
         {
             for (int j = 0; j < polynomial2.m_coefficients.size(); j++)
@@ -141,10 +150,22 @@ public:
         }
         return Polynomial(polynomial_multiply);
     }
-    Polynomial operator/(const int division)
+    Polynomial operator/(const double &division)
     {
         std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), this->m_coefficients.begin(), std::bind(std::divides<int>(), std::placeholders::_1, division));
         return *this;
+    }
+    Polynomial operator+(const Polynomial &polynomial2) const
+    {
+        std::vector<double> sum(this->m_coefficients.size());
+        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), polynomial2.m_coefficients.begin(), sum.begin(), std::plus<int>());
+        return Polynomial(sum);
+    }
+    Polynomial operator-(const Polynomial &polynomial2) const
+    {
+        std::vector<double> not_sum(this->m_coefficients.size());
+        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), polynomial2.m_coefficients.begin(), not_sum.begin(), std::minus<int>());
+        return Polynomial(not_sum);
     }
     Polynomial operator+()
     {
@@ -154,45 +175,41 @@ public:
     {
         return *this * -1;
     }
-    Polynomial operator+(const Polynomial &polynomial2)
+    void operator+=(const Polynomial &polynomial2)
     {
-        std::vector<int> sum(this->m_coefficients.size());
-        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), polynomial2.m_coefficients.begin(), sum.begin(), std::plus<int>());
-        return Polynomial(sum);
+        *this = *this + polynomial2;
     }
-    Polynomial operator-(const Polynomial &polynomial2)
+    void operator-=(const Polynomial &polynomial2)
     {
-        std::vector<int> not_sum(this->m_coefficients.size());
-        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), polynomial2.m_coefficients.begin(), not_sum.begin(), std::minus<int>());
-        return Polynomial(not_sum);
+        *this = *this - polynomial2;
     }
-    Polynomial &operator+=(Polynomial polynomial2)
+    void operator*=(const double &multiply)
     {
-        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), polynomial2.m_coefficients.begin(), this->m_coefficients.begin(), std::plus<int>());
-        return *this;
+        *this = *this * multiply;    
     }
-    Polynomial &operator-=(Polynomial polynomial2)
+    void operator*=(const Polynomial &polynomial2)
     {
-        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), polynomial2.m_coefficients.begin(), this->m_coefficients.begin(), std::minus<int>());
-        return *this;
+        std::vector<double> polynomial_multiply(this->m_coefficients.size() + polynomial2.m_coefficients.size() - 1);
+        for (int i = 0; i < this->m_coefficients.size(); i++)
+        {
+            for (int j = 0; j < polynomial2.m_coefficients.size(); j++)
+            {
+                polynomial_multiply[i + j] += this->m_coefficients[i] * polynomial2.m_coefficients[j];
+            }
+        }
+        *this = polynomial_multiply;
     }
-    Polynomial &operator*=(const int multiply)
+    void operator/=(const double &division)
     {
-        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), this->m_coefficients.begin(), std::bind(std::multiplies<int>(), std::placeholders::_1, multiply));
-        return *this;
-    }
-    Polynomial &operator/=(const int division)
-    {
-        std::transform(this->m_coefficients.begin(), this->m_coefficients.end(), this->m_coefficients.begin(), std::bind(std::divides<int>(), std::placeholders::_1, division));
-        return *this;
+        *this = *this / division;
     }
 };
 
 int main()
 {
-    Polynomial pol{{2, 1, 1, 2, 2}};
+    Polynomial pol{{2, 2, 2, 2, 2}};
     Polynomial pol2{{1, 1, 1, 1, 1}};
-    pol -= pol2 * 2;
+    pol = pol / 2;
     std::cout << pol;
     return 0;
 }
